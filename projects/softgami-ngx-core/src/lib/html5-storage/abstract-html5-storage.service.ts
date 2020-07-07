c50f1644feca6492733465b18cb4160ff2c27934
Luz <lslz@gft.com>
2020-07-07 14:02:55 -0300

    fix(html5Storage): removing Storage type.

--- projects/softgami-ngx-core/src/lib/html5-storage/abstract-html5-storage.service.ts
+++ projects/softgami-ngx-core/src/lib/html5-storage/abstract-html5-storage.service.ts
@@ -13,7 +13,7 @@ export abstract class AbstractHtml5StorageService {
 
     constructor(
         @Inject('shouldEncrypt') @Optional() public shouldEncrypt?: boolean,
-        @Inject('storage') @Optional() public storage?: Storage,
+        @Inject('storage') @Optional() public storage?: any,
     ) {
 
         this.privateKey = '28139f5bfdf08fe0a57cadb9625c28785dad6d46b6a5df0a69c5e0349e79c680ac4cc9a850bd402e15f64403d6' +
