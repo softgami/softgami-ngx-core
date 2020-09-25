31096b284e0b85c996e9dbef23c55cc764caf5c0
Softgami <softgami@gmail.com>
2020-09-25 13:15:55 -0300

    fix(validators): fixing memory leak of subscribing on every execution of match other validator.

--- projects/softgami-ngx-core/src/lib/softgami-shared/validators/match-other.validator.ts
+++ projects/softgami-ngx-core/src/lib/softgami-shared/validators/match-other.validator.ts
@@ -2,17 +2,22 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function MatchOtherValidator(otherControl: AbstractControl): ValidatorFn {
 
+    let mainControl: AbstractControl;
+
+    otherControl.valueChanges.subscribe(() => {
+        if (mainControl) {
+            mainControl.updateValueAndValidity();
+        }
+    });
+
     return (control: AbstractControl): ValidationErrors | null => {
 
+        mainControl = control;
         const error: ValidationErrors = {
             matchOther: true,
         };
 
         if (!otherControl) return null;
-
-        otherControl.valueChanges.subscribe(() => {
-            control.updateValueAndValidity();
-        });
 
         if (otherControl.value === control.value) {
             return null;
