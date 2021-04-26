d082573d480410eed7bd9d947f8e2bbb7dac7219
Softgami <softgami@gmail.com>
2021-04-26 13:43:12 -0300

    feat(core): crud repository methods allow return null values.

--- projects/softgami-ngx-core/src/lib/softgami-core/repository/abstract-base-crud-repository.service.ts
+++ projects/softgami-ngx-core/src/lib/softgami-core/repository/abstract-base-crud-repository.service.ts
@@ -3,10 +3,10 @@ import { Thing } from 'softgami-ts-core';
 
 export abstract class AbstractBaseCRUDRepositoryService<T> {
 
-    abstract find(thing?: Thing): Observable<T[]>;
-    abstract findOne(id: string, appInstanceId: string): Observable<T>;
-    abstract save(object: T): Observable<T>;
-    abstract update(object: T): Observable<T>;
+    abstract find(thing?: Thing): Observable<T[] | null>;
+    abstract findOne(id: string, appInstanceId: string): Observable<T | null>;
+    abstract save(object: T): Observable<T | null>;
+    abstract update(object: T): Observable<T | null>;
     abstract remove(id: string, appInstanceId: string): Observable<void>;
 
 }
